f72c3b7dd6be0d5863626da576b2c815cbdd046e
NemesLaszlo <wow.laszlo@gmail.com>
2020-01-19 18:41:26 +0100

    React Fragment

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -7,10 +7,15 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
 
