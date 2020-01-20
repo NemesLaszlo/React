31fad08f6efb5398cf5fe291f48d98c706cfcf23
NemesLaszlo <wow.laszlo@gmail.com>
2020-01-20 19:44:49 +0100

    todolist-app - handleChange implementation

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import TodoItem from './components/TodoItem';
+import todosData from './datas/todosData';
 import './style.css';
 
 class App extends Component {
-  state = {};
+  state = {
+    todos: todosData
+  };
+
+  List_todoItems() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} onChange={this.handleChange} />
+    ));
+    return todoItems;
+  }
+
+  handleChange = id => {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos
+      };
+    });
+  };
+
   render() {
-    return (
-      <div className="todo-list">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-      </div>
-    );
+    return <div className="todo-list">{this.List_todoItems()}</div>;
   }
 }
 
--- todolist-app/src/components/TodoItem.jsx
+++ todolist-app/src/components/TodoItem.jsx
@@ -5,8 +5,12 @@ class TodoItem extends Component {
   render() {
     return (
       <div className="todo-item">
-        <input type="checkbox" />
-        <p>Placeholder text here</p>
+        <input
+          type="checkbox"
+          checked={this.props.item.completed}
+          onChange={() => this.props.onChange(this.props.item.id)}
+        />
+        <p>{this.props.item.text}</p>
       </div>
     );
   }
