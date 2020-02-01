42d01d658f8c564657a13796dfaedb51e7df0af1
NemesLaszlo <wow.laszlo@gmail.com>
2020-02-01 15:12:48 +0100

    Backend - Firebase addUserDetails, GetAuthUserDetails, getPost and commenting implementation

--- social media/socialApp-functions/functions/handlers/users.js
+++ social media/socialApp-functions/functions/handlers/users.js
@@ -1,5 +1,9 @@
 const { admin, db } = require('../util/admin');
-const { validateSignUpData, validateLogInData } = require('../util/validators');
+const {
+  validateSignUpData,
+  validateLogInData,
+  reduceUserDetails
+} = require('../util/validators');
 const config = require('../util/config');
 const firebase = require('firebase');
 firebase.initializeApp(config);
@@ -90,6 +94,49 @@ exports.logIn = (req, res) => {
       } else {
         return res.status(500).json({ error: err.code });
       }
+    });
+};
+
+// Add user details.
+exports.addUserDetails = (req, res) => {
+  let userDetails = reduceUserDetails(req.body);
+
+  db.doc(`/users/${req.user.handle}`)
+    .update(userDetails)
+    .then(() => {
+      return res.json({ message: 'Details added successfully!' });
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+// Get own user details.
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {};
+
+  db.doc(`/users/${req.user.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        userData.credentials = doc.data();
+        return db
+          .collection('likes')
+          .where('userHandle', '==', req.user.handle)
+          .get();
+      }
+    })
+    .then(data => {
+      userData.likes = [];
+      data.forEach(doc => {
+        userData.likes.push(doc.data());
+      });
+      return res.json(userData);
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).json({ error: err.code });
     });
 };
 
--- social media/socialApp-functions/functions/index.js
+++ social media/socialApp-functions/functions/index.js
@@ -2,16 +2,33 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signUp, logIn, uploadImage } = require('./handlers/users');
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream
+} = require('./handlers/screams');
+
+const {
+  signUp,
+  logIn,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require('./handlers/users');
+
 const FirebaseAuth = require('./util/FirebaseAuth');
 
 // Screams routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FirebaseAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+app.post('/scream/:screamId/comment', FirebaseAuth, commentOnScream);
 // Users routes
 app.post('/signup', signUp);
 app.post('/login', logIn);
 app.post('/user/image', FirebaseAuth, uploadImage);
+app.post('/user', FirebaseAuth, addUserDetails);
+app.get('/user', FirebaseAuth, getAuthenticatedUser);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
